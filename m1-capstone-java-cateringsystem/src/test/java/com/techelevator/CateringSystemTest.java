9471a44d80eba54e75eced8c8c63ba1cbee59e3f
Chris Linscott <chris.linscott00@gmail.com>
2022-06-12 16:44:33 -0400

    capstone update

--- m1-capstone-java-cateringsystem/src/test/java/com/techelevator/CateringSystemTest.java
+++ m1-capstone-java-cateringsystem/src/test/java/com/techelevator/CateringSystemTest.java
@@ -1,49 +1,65 @@
 package com.techelevator;
 
+import com.techelevator.items.CateringItem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CateringSystemTest {
     private CateringSystem cateringSystem;
 
     @Before
-    public void setup(){
-        cateringSystem= new CateringSystem();
-    }
-
-
-
-    @Test
-    public void update_shopping_balance_zero_quantity(){
-
-        float testBalance= cateringSystem.getCurrentAccountBalance();
-        float actualBalance= cateringSystem.updatingBalanceAfterShopping(5,0);
-        Assert.assertEquals(testBalance, actualBalance,.009);
+    public void setup() {
+        cateringSystem = new CateringSystem();
     }
 
 
     @Test
-    public void update_shopping_balance_with_quantity(){
+    public void update_shopping_balance_zero_quantity() {
 
-        float testBalance= -10;
-        float actualBalance= cateringSystem.updatingBalanceAfterShopping(5,2);
-        Assert.assertEquals(testBalance, actualBalance,.009);
+        float testBalance = cateringSystem.getCurrentAccountBalance();
+        float actualBalance = cateringSystem.updatingBalanceAfterShopping(5, 0);
+        Assert.assertEquals(testBalance, actualBalance, .009);
+    }
+
+
+    @Test
+    public void update_shopping_balance_with_quantity() {
+
+        float testBalance = -10;
+        float actualBalance = cateringSystem.updatingBalanceAfterShopping(5, 2);
+        Assert.assertEquals(testBalance, actualBalance, .009);
     }
 
     @Test
-    public void reset_Balance(){
-        float testBalance= 0;
-        float actualBalance= cateringSystem.resetBalance();
-        Assert.assertEquals(testBalance,actualBalance, .009);
+    public void reset_Balance() {
+        float testBalance = 0;
+        float actualBalance = cateringSystem.resetBalance();
+        Assert.assertEquals(testBalance, actualBalance, .009);
     }
 
     @Test
-    public void adding_more_than_500_at_one_time(){
-        float testBalance=0;
+    public void adding_more_than_500_at_one_time() {
+        float testBalance = 0;
 
-   //     float actualBalance = cateringSystem.addMoney(501);
+        //     float actualBalance = cateringSystem.addMoney(501);
     }
+
+//    @Test
+//    public void grabbing_quantity_correctly() {
+//
+//        String testCode = "E5";
+//
+//        Map<String, CateringSystem> expectedResult = new TreeMap<>();
+//        expectedResult.put(testCode, cateringSystem);
+//        expectedResult.put(testCode, cateringSystem);
+//
+//        int result = cateringSystem.getQuantity(testCode);
+//        Assert.assertEquals(expectedResult, result);
+//    }
+
 }
-
-
